f0c6f297ac3d43eb9b2433832d55a27352542744
Mohan Nukala <mnukala@gspann.com>
2021-01-20 15:18:51 +0530

    Third commit

--- start.js
+++ start.js
@@ -2,7 +2,7 @@ const https = require('https');
 const http = require('http');
 const fs = require('fs');
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // const options = {
 //     key: fs.readFileSync('key.pem'),
